b4e58368223bbf03768ed9bd132ea128441e8a0f
Nissan <nissan98448@gmail.com>
2024-09-04 20:53:34 +0600

    Add files via upload

---
+++ JavaApplication19/src/javaapplication19/JavaApplication19.java
@@ -0,0 +1,18 @@
+
+package javaapplication19;
+
+/**
+ *
+ * @author DELL
+ */
+public class JavaApplication19 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("hiii");
+        // TODO code application logic here
+    }
+    
+}
