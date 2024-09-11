b0c7e9f054c1f3e6072eb6b6d1a999ee9250e0c7
Nissan <nissan98448@gmail.com>
2024-09-11 15:44:34 +0600

    Add files via upload

---
+++ java1/src/java1/Java1.java
@@ -0,0 +1,38 @@
+
+package java1;
+
+
+public class Java1 {
+/*static void Nissan(int a){
+    System.out.println("inty is:"+a);
+    
+}
+static void Nissan(double a){
+    System.out.println("Double is:"+a);
+}
+    public static void main(String[] args) {
+      Nissan(20);
+      Nissan(20.0);
+    }
+    
+}*/
+    
+    //This code can be done with as much space as possible using multi-line commands
+ 
+     
+     
+   
+
+    public static void main(String args[]){
+     Nissan(20);
+      Nissan(20.0);
+    }
+
+static void Nissan(int a){
+   System.out.println("Int value is : "+a);
+}
+static void Nissan(double a){
+    System.out.println("Double value is : "+a);
+}
+}
+
