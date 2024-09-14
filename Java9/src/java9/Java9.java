a5af32339021f02d28f349c27f17e381839878d8
Nissan <nissan98448@gmail.com>
2024-09-14 20:28:17 +0600

    Add files via upload

---
+++ Java9/src/java9/Java9.java
@@ -0,0 +1,23 @@
+package java9;
+
+import javax.swing.JOptionPane;
+
+public class Java9{
+    public static void main(String args[]){
+        int option=JOptionPane.showConfirmDialog(null,"YES NO Statement");
+        
+        if(option==JOptionPane.YES_OPTION){
+            JOptionPane.showMessageDialog(null,"The YES Statement");
+            System.out.println("I am Nissan");
+        }
+        else 
+            if(option==JOptionPane.NO_OPTION){
+             JOptionPane.showMessageDialog(null, "THe NO Statement");
+                System.out.println("I am The winner");
+            }
+            else{
+                JOptionPane.showConfirmDialog(null,"The CANCEL Statement");
+           System.out.println("Nissan You Can Take Risk Because No Risk no Story");      
+            }
+    }
+}
