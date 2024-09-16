9c84af32fd627682bcf9282bea86203f7f36348b
Nissan <nissan98448@gmail.com>
2024-09-16 21:16:30 +0600

    Update Java16.java

--- Java16/src/java16/Java16.java
+++ Java16/src/java16/Java16.java
@@ -1,20 +1,34 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package java16;
 
-/**
- *
- * @author DELL
- */
-public class Java16 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+class A{
+    public void MethodA(){
+            System.out.println("Method A Working");  
     }
-    
 }
+   class B extends A {
+    public void MethodB(){
+       
+     System.out.println("Method B Working");
+    }
+   }
+   class C extends A {
+    public void MethodC(){
+  System.out.println("Method C Working");   
+    }
+   }
+    class D extends A{
+        public void MethodD(){
+            System.out.println("Method D Working");
+        }
+    }
+   public class Java16{
+       public static void main(String[] args) {
+           B ob=new B();
+           C ob1=new C();
+           D ob2=new D();
+           ob.MethodA();
+           ob.MethodB();
+           ob1.MethodC();
+           ob2.MethodD();
+       }
+   }
