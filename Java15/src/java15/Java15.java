f1732635e524741df2e95539a2b4ca6dbaa27195
Nissan <nissan98448@gmail.com>
2024-09-16 17:47:40 +0600

    Add files via upload

---
+++ Java15/src/java15/Java15.java
@@ -0,0 +1,38 @@
+
+package java15;
+
+// Hybridal Inheritence
+class Animal{
+    public void Eat(){
+        System.out.println("");
+    }
+}
+interface Bird{
+    void Fly();
+}
+interface Cat{
+    void Sleep();
+}
+class Bat extends Animal implements Bird,Cat{
+    
+    @Override
+    public void Eat(){
+        System.out.println("Animan is better than fake people");
+    }
+    @Override
+    public void Fly(){
+        System.out.println("Birds can Fly");
+    }
+    @Override
+    public void Sleep(){
+        System.out.println("Now cat is Sleeping");
+    } 
+}
+public class Java15{
+    public static void main(String[] args) {
+     Bat sc=new Bat();
+     sc.Eat();
+     sc.Fly();
+     sc.Sleep();
+    }
+}
